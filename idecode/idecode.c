6cee560ef2018daf79794e01e78731f59b34e527
tgidave <dave@tgi.com>
2019-05-10 10:20:13 -0600

    Changed to exit with error code 1 if no file found during -f processing.

--- idecode/idecode.c
+++ idecode/idecode.c
@@ -85,7 +85,7 @@ int main(int argc, char** argv) {
         case 'f':
           if (stat(argv[argIx + 1], &fileStat) < 0) {
             printf("No file found.\n");
-            return (0);
+            exit(1);
           }
           if (getDataByFile(&argv[argIx + 1]) != 0) {
             exit(1);
